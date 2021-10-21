dd1cd57a7f1e3ef4a50ee1f0cb7c35eefe50005c
Ahrorbek <akhrorcodrr@gmail.com>
2021-10-21 15:51:08 +0500

    fully finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,97 @@
-import React, { useState } from "react";
-import { Container, IconContainer, MobileContainer,Icon } from "./AppStyle";
+import React, { useState, useContext } from "react";
+import {
+  Container,
+  IconContainer,
+  MobileContainer,
+  Icon,
+  CatImg,
+  ImgDiv,
+  TitleImg,
+  Desc,
+  Item,
+  Title,
+  Line,
+  ToggleMode,
+} from "./AppStyle";
+import Cat from "./cat.jpg";
+import { ModeContext } from "./context";
 
-const App =()=>{
+const App = () => {
+  const [mode, setMode] = useState(false);
+  const [theme, setTheme] = useContext(ModeContext);
+  const dark = {
+    icon: "black",
+    title: "black",
+    background: "white",
+  };
 
-  return(
-    <Container>
-       <MobileContainer>
-         <IconContainer>
-          <Icon.Arrow /> 
-         </IconContainer>
-       </MobileContainer>
+  const light ={
+    icon: 'white',
+    title:'white',
+    background: 'black',
+  }
+
+  const setIsDarkMode = () => {
+    setMode(!mode);
+    setTheme(mode? dark: light)
+  };
+  return (
+    <Container >
+      <MobileContainer color={theme.background}>
+        <IconContainer>
+          <Icon.Arrow color={theme.icon}/>
+          <Icon.Switch color={theme.icon}/>
+        </IconContainer>
+        <ImgDiv>
+          <CatImg src={Cat} alt="cat" />
+          <TitleImg color={theme.title}>Catty</TitleImg>
+          <Desc color={theme.title}>CatLover</Desc>
+        </ImgDiv>
+        {/* darkMode */}
+        <Item>
+          <Icon.Moon color={theme.icon}/>
+          <Title color={theme.title}>Dark Mode</Title>
+          <ToggleMode onChange={setIsDarkMode} checked={mode} size={40} />
+        </Item>
+        {/* Grid */}
+        <Item>
+          <Icon.Grid color={theme.icon}/>
+          <Title color={theme.title}>Story</Title>
+        </Item>
+        {/* Chat */}
+        <Item>
+          <Icon.User color={theme.icon}/>
+          <Title color={theme.title}>Chat heads</Title>
+        </Item>
+        {/* Groups */}
+        <Item>
+          <Icon.Userplus color={theme.icon}/>
+          <Title color={theme.title}>Groups</Title>
+        </Item>
+        <Line></Line>
+        {/* Media */}
+        <Item>
+          <Icon.Media color={theme.icon}/>
+          <Title color={theme.title}>Media and photos</Title>
+        </Item>
+        {/* Settings */}
+        <Item>
+          <Icon.Setting color={theme.icon}/>
+          <Title color={theme.title}>Settings and Privacy</Title>
+        </Item>
+        {/* Comments */}
+        <Item>
+          <Icon.Comments color={theme.icon} />
+          <Title color={theme.title}>Help Centre</Title>
+        </Item>
+        {/* Notifications */}
+        <Item>
+          <Icon.Notification color={theme.icon} />
+          <Title color={theme.title}>Notifications</Title>
+        </Item>
+      </MobileContainer>
     </Container>
-
-  )
-}
-
+  );
+};
 
 export default App;
--- src/AppStyle.js
+++ src/AppStyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { Settings } from "@styled-icons/evaicons-solid/Settings2";
-import { Arrowleft } from "@styled-icons/material-rounded/KeyboardArrowLeft";
+import { Settings2 } from "@styled-icons/evaicons-solid/Settings2";
+import { KeyboardArrowLeft } from "@styled-icons/material/KeyboardArrowLeft";
 import { Switch } from "@styled-icons/icomoon/Switch";
 import { Moon } from "@styled-icons/boxicons-solid/Moon";
 import { Grid } from "@styled-icons/bootstrap/Grid";
@@ -9,8 +9,12 @@ import { UserPlus } from "@styled-icons/boxicons-regular/UserPlus";
 import { PermMedia } from "@styled-icons/material-sharp/PermMedia";
 import { CommentDots } from "@styled-icons/boxicons-regular/CommentDots";
 import { Notifications } from "@styled-icons/ionicons-sharp/Notifications";
-import {Zap} from '@styled-icons/Adobe'
 
+import Toggle from 'react-dark-mode-toggle'
+
+export const ToggleMode = styled(Toggle)`
+  margin-left: auto;
+`;
 
 export const Container = styled.div`
   display: flex;
@@ -24,24 +28,137 @@ export const Container = styled.div`
 export const MobileContainer = styled.div`
   width: 400px;
   height: 600px;
-  background-color: whitesmoke;
+  min-width: 400px;
+  min-height: 600px;
+  background-color: ${({color})=> color};
   box-shadow: 5px 5px 12px 6px rgba(0, 0, 0, 0.25);
 `;
 
-
 export const IconContainer = styled.div`
+  display: flex;
   width: 100%;
   padding: 20px;
-
 `;
 
-export const Icon = styled.div`
-  
+export const Icon = styled.div``;
+
+Icon.Arrow = styled(KeyboardArrowLeft)`
+  color: ${({color})=> color};
+  width: 30px;
+  height: 30px;
+  cursor: pointer;
 `;
 
-Icon.Arrow = styled(Arrowleft)`
-color: black;
-width: 30px;
-height: 30px;
-cursor: pointer;
-`  
+Icon.Switch = styled(Switch)`
+  color: ${({color})=> color};
+  width: 18px;
+  height: 18px;
+  cursor: pointer;
+  margin-left: auto;
+`;
+
+export const ImgDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+export const CatImg = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 2px solid lightgrey;
+`;
+
+export const TitleImg = styled.div`
+  color: ${({color})=> color};
+  font-size: 22px;
+  font-weight: bold;
+  font-family: SF Pro Display;
+`;
+
+export const Desc = styled.div`
+  color: ${({color})=> color};
+  font-size: 14px;
+  font-weight: normal;
+  font-family: SF Pro Display;
+`;
+
+export const Item = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 30px;
+`;
+
+export const Title = styled.div`
+  color: ${({color})=> color};
+  font-size: 16px;
+  font-weight: normal;
+  font-family: SF Pro Display;
+  padding-left: 15px;
+`;
+
+export const Line = styled.div`
+  width: 75%;
+  height: 2px;
+  background-color: #e9e9e9;
+  margin: 10px;
+  box-sizing: border-box;
+  /* padding: 10px 0; */
+`;
+
+Icon.Moon = styled(Moon)`
+  width: 20px;
+  height: 20px;
+  color: ${({color})=> color};
+`;
+
+Icon.Grid = styled(Grid)`
+  width: 20px;
+  height: 20px;
+  color: #fbb65c;
+`;
+
+Icon.User = styled(UserCheck)`
+  width: 20px;
+  height: 20px;
+  color: #fbb65c;
+`;
+
+Icon.Userplus = styled(UserPlus)`
+  width: 20px;
+  height: 20px;
+  color: #36e3ff;
+`;
+
+Icon.Userplus = styled(UserPlus)`
+  width: 20px;
+  height: 20px;
+  color: #36e3ff;
+`;
+
+Icon.Media = styled(PermMedia)`
+  width: 20px;
+  height: 20px;
+  color: #5476ff;
+`;
+
+Icon.Setting = styled(Settings2)`
+  width: 20px;
+  height: 20px;
+  color: #5476ff;
+`;
+
+Icon.Comments = styled(CommentDots)`
+  width: 20px;
+  height: 20px;
+  color: #0bc630;
+`;
+
+Icon.Notification = styled(Notifications)`
+  width: 20px;
+  height: 20px;
+  color: #a394c2 ;
+`;
